6a049ef6146d68b96af05969c1e60f0b76c070a7
Valyutik <lind-2004@mail.ru>
2023-07-16 21:30:25 +0300

    Добавил детектор интерактивных объектов

--- Assets/Soulcutter/Scripts/Character/InteractionObjectDetector.cs
+++ Assets/Soulcutter/Scripts/Character/InteractionObjectDetector.cs
@@ -1,19 +1,25 @@
+using System;
+using Soulcutter.Scripts.TreeChopping;
 using UnityEngine;
 
 namespace Soulcutter.Scripts.Character
 {
     public class InteractionObjectDetector : MonoBehaviour
     {
-        
+        public event Action<Wood> OnTriggerWithWood;
+        public event Action OnTriggerExit;
         
         private void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log(other);
+            if (other.TryGetComponent<Wood>(out var tree))
+            {
+                OnTriggerWithWood?.Invoke(tree);
+            }
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
-            Debug.Log(other);
+            OnTriggerExit?.Invoke();
         }
     }
 }
